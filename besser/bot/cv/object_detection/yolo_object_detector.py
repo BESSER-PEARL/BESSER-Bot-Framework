5516152d372126d6f727e18cb1dff0eb975640cd
mgv99 <gomezmarcos99@gmail.com>
2024-10-22 11:22:07 +0200

    fix retrieve class names in yolo

--- besser/bot/cv/object_detection/yolo_object_detector.py
+++ besser/bot/cv/object_detection/yolo_object_detector.py
@@ -14,90 +14,6 @@ if TYPE_CHECKING:
     from besser.bot.core.bot import Bot
 
 
-class_names = [
-    "person",
-    "bicycle",
-    "car",
-    "motorbike",
-    "aeroplane",
-    "bus",
-    "train",
-    "truck",
-    "boat",
-    "traffic light",
-    "fire hydrant",
-    "stop sign",
-    "parking meter",
-    "bench",
-    "bird",
-    "cat",
-    "dog",
-    "horse",
-    "sheep",
-    "cow",
-    "elephant",
-    "bear",
-    "zebra",
-    "giraffe",
-    "backpack",
-    "umbrella",
-    "handbag",
-    "tie",
-    "suitcase",
-    "frisbee",
-    "skis",
-    "snowboard",
-    "sports ball",
-    "kite",
-    "baseball bat",
-    "baseball glove",
-    "skateboard",
-    "surfboard",
-    "tennis racket",
-    "bottle",
-    "wine glass",
-    "cup",
-    "fork",
-    "knife",
-    "spoon",
-    "bowl",
-    "banana",
-    "apple",
-    "sandwich",
-    "orange",
-    "broccoli",
-    "carrot",
-    "hot dog",
-    "pizza",
-    "donut",
-    "cake",
-    "chair",
-    "sofa",
-    "pottedplant",
-    "bed",
-    "diningtable",
-    "toilet",
-    "tvmonitor",
-    "laptop",
-    "mouse",
-    "remote",
-    "keyboard",
-    "cell phone",
-    "microwave",
-    "oven",
-    "toaster",
-    "sink",
-    "refrigerator",
-    "book",
-    "clock",
-    "vase",
-    "scissors",
-    "teddy bear",
-    "hair drier",
-    "toothbrush"
-]
-
-
 class YOLOObjectDetector(ObjectDetector):
     """The YOLO Object Detector.
 
@@ -134,7 +50,7 @@ class YOLOObjectDetector(ObjectDetector):
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 score = math.ceil((box.conf[0] * 100)) / 100
                 cls = box.cls[0]
-                name = class_names[int(cls)]
+                name = self.yolo.names[int(cls)]
                 image_object: ImageObject = self._cv_engine._bot.get_image_object(name)
                 if image_object:
                     image_object_prediction: ImageObjectPrediction = ImageObjectPrediction(
