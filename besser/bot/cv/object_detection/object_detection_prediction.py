7b7fbfd30b951a668927af38ece9fbc91ac81080
mgv99 <gomezmarcos99@gmail.com>
2024-10-23 19:49:54 +0200

    add get_image_object_predictions method in ObjectDetectionPrediction

--- besser/bot/cv/object_detection/object_detection_prediction.py
+++ besser/bot/cv/object_detection/object_detection_prediction.py
@@ -67,6 +67,22 @@ class ObjectDetectionPrediction:
         self.model_name: str = model_name
         self.image_input_name: str = image_input_name
 
+    def get_image_object_predictions(self, name: str) -> list[ImageObjectPrediction]:
+        """Get all the image object predictions with a specific image object name.
+
+        Args:
+            name (str): the name of the image objects to get
+
+        Returns:
+            list[ImageObjectPrediction]: the image object predictions whose image object's name matches with the
+            provided name
+        """
+        image_object_predictions: list[ImageObjectPrediction] = []
+        for image_object_prediction in self.image_object_predictions:
+            if image_object_prediction.image_object.name == name:
+                image_object_predictions.append(image_object_prediction)
+        return image_object_predictions
+
 
 class ObjectDetectionPredictionEncoder(json.JSONEncoder):
     """Encoder for the :class:`ObjectDetectionPrediction` class.
